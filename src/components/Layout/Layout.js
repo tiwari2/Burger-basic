574ba6547b2aba65aa34ba28b9d83756ea323400
Harsh-tiwari <harsh@aiknow.in>
2020-04-21 20:04:47 +0530

    add price in the Modal , Toolbar , Logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
-import "./Layout.css";
+import classes from "./Layout.css";
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const Layout = (props) => (
     <Aux>
-        <div>Toolbar , SideDrawer, Backdrop</div>
-        <main className="Content">
+        <Toolbar />
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
